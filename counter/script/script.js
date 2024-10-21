c427bfdeee97918e261076eec2cedf156d08ec9c
2301175Tanishi <2301175@s.asojuku.ac.jp>
2024-10-21 11:40:58 +0900

    カウント

--- counter/script/script.js
+++ counter/script/script.js
@@ -3,6 +3,15 @@ new Vue({
     el: '#app',
     data() {
         return {
+            count:1
         };
+    },
+    methods: {
+        increment(){
+            this.count * 2;
+        },
+        decremnt(){
+            this.count / 2;
+        }
     }
 });
